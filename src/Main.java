9035efdbdba671856ae195984776d420d70fc309
Cyclic <cyclic007dev@gmail.com>
2024-09-05 10:41:45 -0500

    logs are hard

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
 import hashing.HashUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        File log = new File("log_"+timestamp.toString().replaceAll(" ","_").replaceAll("\\.",",").replaceAll("\\.",",")+".txt");
+        System.out.println(log.getAbsolutePath());
+        log.createNewFile();
 
 
 
 
-        System.out.println(number);
     }
+//    public static void main(String[] args)
+//    {
+//
+//        try {
+//            File Obj = new File("myfile.txt");
+//            if (Obj.createNewFile()) {
+//                System.out.println("File created: "
+//                        + Obj.getName());
+//            }
+//            else {
+//                System.out.println("File already exists.");
+//            }
+//        }
+//        catch (IOException e) {
+//            System.out.println("An error has occurred.");
+//            e.printStackTrace();
+//        }
+//    }
 }
---
+++ src/keySystem/KeyRing.java
@@ -0,0 +1,4 @@
+package keySystem;
+
+public class KeyRing {
+}
